7f492e1e07711a9380aa317a49954a5982841040
NALLAMILLI VINEETHA DEVIKA <84488013+vineethadevika@users.noreply.github.com>
2024-07-12 06:37:18 +0530

    BigInteger.java

---
+++ BigInteger.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        java.math.BigInteger a=sc.nextBigInteger();
+        java.math.BigInteger b=sc.nextBigInteger();
+        System.out.println(a.add(b));
+        System.out.println(a.multiply(b));
+    }
+}
